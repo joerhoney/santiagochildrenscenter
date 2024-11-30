c2f84b7f1379cfd3267c654e4695e88fb463532d
joerhoney <6787748+joerhoney@users.noreply.github.com>
2024-11-30 00:56:03 -0800

    Putting layout together from mockup.

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,68 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //  const [count, setCount] = useState(0);
+  //  <div className="card">
+  //    <button onClick={() => setCount((count) => count + 1)}>
+  //      count is {count}
+  //    </button>
+  //  </div>;
 
   return (
     <>
-      <section>
-        <div>
-          <h1>Santiago Childrens Center</h1>
-        </div>
-        <div>2. image</div>
-        <div>
-          3. A headline that emphasizes the value, result or transformation Your
-          services offer.
-        </div>
-        <div>
-          4. A tagline to elaborate on the headline by clarifying how you get
-          such results.
-        </div>
-        <div>5. A CTA (call to action): Call (/Text) Us @ 714-309-4237</div>
-        <div>6. 5 stars on Yelp</div>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-        </div>
-      </section>
-      <section>Benefits</section>
-      <section>Testimonials</section>
-      <section>FAQs</section>
-      <section>Information</section>
-      <section>CTA</section>
+      <div
+        className="bg-no-repeat bg-center"
+        style={{
+          backgroundImage: 'url("./src/assets/scc-mock-1S.jpg")',
+          height: "4792px",
+        }}
+      >
+        <section className="p-5 m-0">
+          <img
+            src="src/assets/Santiago-Childrens-Center-logo.svg"
+            alt="Santiago Childrens Center logo"
+            className="logo pb-5"
+          />
+          <h1 className="text-deepgreen font-sans text-4xl">
+            Your child, <br />
+            nurtured <br />
+            by nature
+          </h1>
+          <p className="py-3">
+            A daycare where children are respected, encouraged, and developed in
+            a warm, safe country atmosphere.
+          </p>
+          <a className="btn btn--shape my-2 mx-auto max-w-[240px]" href="#">
+            EMAIL US
+          </a>
+          <div className="text-center text-color-aqua">
+            <div className="font-bold">OR CALL</div>
+            <div className="font-bold text-3xl">714-309-4237</div>
+          </div>
+          <div className="shape">
+            <img src="./src/assets/o-0.jpg" alt="" />
+          </div>
+          <img src="./src/assets/o-1.jpg" alt="" />
+          <img src="./src/assets/o-2.jpg" alt="" />
+          <img src="./src/assets/o-3.jpg" alt="" />
+        </section>
+        <section>
+          <h2>Benefits</h2>
+        </section>
+        <section>
+          <h2>Testimonials</h2>
+        </section>
+        <section>
+          <h2>FAQs</h2>
+        </section>
+        <section>
+          <h2>Information</h2>
+        </section>
+        <section>
+          <div>EMAIL US</div>
+          <div>OR CALL 714-309-4237</div>
+        </section>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        deepgreen: "#121f26",
+        forest: "#3D696D",
+        white: "#ffffff",
+        aqua: "#30b9b1",
+        rose: "#ffd6db",
+        peach: "#fff5cc",
+        "gray-dark": "#273444",
+        gray: "#8492a6",
+        "gray-light": "#d3dce6",
+      },
+      fontFamily: {
+        sans: ["Host Grotesk", "sans-serif"],
+        serif: ["Libre Baskerville", "serif"],
+      },
+      zIndex: {
+        "1px": "1px",
+      },
+      // content: {
+      //   empty: [""],
+      // },
+    },
   },
   plugins: [],
 };
